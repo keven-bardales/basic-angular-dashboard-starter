3edd43e7313e61849c3bae2faa4a353edee55822
keven-bardales <keven.bardales@gmail.com>
2025-05-02 23:41:58 -0600

    fix: ts config and v1 routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: 'v1',
+                loadChildren: () => import('@v1/version-1.routes').then(m => m.default)
+            }
+        ]
+    }
+];
